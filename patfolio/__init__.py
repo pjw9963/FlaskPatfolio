9a27df207cf5f6fa86521c33bbb42d24b11b3ed0
pjw9963 <pjw9963@rit.edu>
2020-03-02 13:58:02 -0500

    added admin page

--- patfolio/__init__.py
+++ patfolio/__init__.py
@@ -1,6 +1,23 @@
 from flask import Flask
+from flask_admin import Admin, AdminIndexView
+from flask_sqlalchemy import SQLAlchemy
+from flask_admin.contrib.sqla import ModelView
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
 
 from patfolio import routes
+from patfolio.models import User
+
+
+class MyAdminIndexView(AdminIndexView):
+    def is_accessible(self):
+        return False
+
+
+admin = Admin(app, name='Admin Console', index_view=MyAdminIndexView())
+
+admin.add_view(ModelView(User, db.session))
---
+++ patfolio/models.py
@@ -0,0 +1,9 @@
+from patfolio import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return 'User: ' + self.username
